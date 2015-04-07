c0e5a244fae95bfcb8727303f0970ef35e5cb0ca
Roman Jouravkov <roma1990may@rambler.ru>
2015-04-07 10:12:21 +0300

    Initial Commit

---
+++ ITRexTest/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  ITRexTest
+//
+//  Created by Hopreeeeenjust on 07.04.15.
+//  Copyright (c) 2015 Hopreeeeenjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
